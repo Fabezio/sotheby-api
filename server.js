3472e7c196cf6e1da9a52f28d068ce27fd9d85bf
fabezio <fabezio@outlook.fr>
2020-10-15 21:42:56 +0200

    ajout affichage par peinture

--- server.js
+++ server.js
@@ -20,57 +20,60 @@ mongoose.connect('mongodb://localhost/sothebyDB', {useNewUrlParser: true});
 
 const paintingSchema = new mongoose.Schema ({
     name: String,
+    imgUrl: String,
     author: String,
     price: Number
 })
 const Painting = mongoose.model("paintingSold", paintingSchema)
 
 const Joconde = new Painting ({
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg",
     name: "La Joconde",
     author: "Da Vinci",
     price: 109
 })
 const Guernica = new Painting ({
+    imgUrl: "https://img.lemde.fr/2017/04/26/0/256/3033/1516/1440/720/60/0/6fc546e_29800-o4vvkx.p5iy17cik9.jpg",
     name: "Guernica",
     author: "Picasso",
     price: 250
 })
 const Athenes = new Painting ({
+    imgUrl: "https://cdn.pixabay.com/photo/2016/01/16/18/30/art-school-of-athens-1143741_1280.jpg",
     name: "L'école d'Athènes",
     author: "Raphael",
     price: 300
 })
 const Creation = new Painting ({
+    imgUrl: "https://cdn.unitycms.io/image/ocroped/1200,1200,1000,1000,0,0/SpPAkr26Fbg/1FbXscSJq5HAdtmaazDrjl.jpg",
     name: "La Création d'Adam",
     author: "Michel-Ange",
     price: 400
 })
 const Cene = new Painting ({
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Leonardo_da_Vinci_%281452-1519%29_-_The_Last_Supper_%281495-1498%29.jpg/1920px-Leonardo_da_Vinci_%281452-1519%29_-_The_Last_Supper_%281495-1498%29.jpg",
     name: "La Cène",
     author: "Da Vinci",
     price: 1000
 })
+// Painting.updateOne({name: "La Grenouillère"}, {imgUrl: "https://www.connaissancedesarts.com/wp-content/thumbnails/uploads/2019/12/cda19_100ans_renoir_main-tt-width-970-height-545-fill-1-crop-0-bgcolor-ffffff.jpg"}, err => console.log(err || "image modifiée"))
 // Painting.updateOne({name: "L'école d'Athènes"}, {imgUrl: "https://cdn.pixabay.com/photo/2016/01/16/18/30/art-school-of-athens-1143741_1280.jpg"}, err => console.log(err || "mise à jour réussie"))
-// Painting.insertMany([Joconde, Athenes, Creation, Cene, Guernica], err => console.log(err || 'insertions effectuées'))
-// Painting.deleteMany({name: "La Joconde"}, err => console.log(err || 'suppression effectuée'))
-// Painting.deleteMany({name: "La Cène"}, err => console.log(err || 'suppression effectuée'))
-// Painting.deleteMany({name: "La Création d'Adam"}, err => console.log(err || 'suppression effectuée'))
-// Painting.deleteMany({name: "Guernica"}, err => console.log(err || 'suppression effectuée'))
-// Painting.deleteMany({name: "L'école d'Athènes"}, err => console.log(err || 'suppression effectuée'))
+// Painting.insertMany([Joconde, Athenes, Creation, Cene, Guernica], err => console.log(err || 'insertion effectuée'))
+
 // Painting.save([ripo])
 // Painting.find({}, err, data => console.log(err || data))
 
 app.get('/', (req, res) => res.render("index", {}))
-Painting.find().size({}, (err, nb) => console.log(err || nb))
-console.log()
+// Painting.find().size({}, (err, nb) => console.log(err || nb))
+// console.log()
 
 app.get('/paintings', (req, res) => {
     Painting.find({}, (err, peintures) => {
         if (err) console.log(err) 
         // else res.send(peintures)
-        // else res.send("paintings",{ oeuvres: peinture})
+        else res.render ("paintings",{ oeuvres: peintures})
         // const 
-        res.render("paintings", {})
+        // res.render("paintings", {})
 
         // res.send(err || peinture)
     })
@@ -94,6 +97,12 @@ app.post('/paintings', function (req, res) {
     })
     //     // console.log(err || req.body.name)
 
+})
+app.get('/paintings/:name', (req, res) => {
+    // console.log(req.params.name)
+    const thisPaint = Painting.findOne({name: req.params.name}, (err, data) => {
+        return console.log(err || "nom incorrect") || res.render("_name", {data: data})
+    })
 })
 app.delete("/paintings", (req, res) => {
     Painting.deleteMany({}, err => console.log(err || "peintures supprimées"))
